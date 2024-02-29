00fa422129b5483e75af058fc3e126aa628bfdd3
Dioico <74198922+Dioico@users.noreply.github.com>
2024-02-29 20:38:58 +0100

    init repo

---
+++ hello_world.c
@@ -0,0 +1,8 @@
+//
+// Created by Emanuele on 29/02/2024.
+//
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
---
+++ integer_io.c
@@ -0,0 +1,12 @@
+//
+// Created by Emanuele on 29/02/2024.
+//
+#include <stdio.h>
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);
+    printf("Number = %d",testInteger);
+    return 0;
+}
